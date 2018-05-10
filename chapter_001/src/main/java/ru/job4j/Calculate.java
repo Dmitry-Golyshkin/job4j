9a14d5c130be5b98db3857f557147f952d0dae69
Dmitry Golyshkin <mailerema@gmail.com>
2018-05-10 10:53:12 +0300

    1.3.1 Maven Plugins

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,20 +1,27 @@
-package ru.job4j.calculate;
+package ru.job4j;
 
 /**
-* Calculate.
-*
-*@author Dmitry Golyshkin (mailerema@gmail.com)
-**/
-
+ * Calculate.
+ *
+ * @author Dmitry Golyshkin (mailerema@gmail.com)
+ * @version $Id$
+ * @since 10.05.2018
+ */
 public class Calculate {
-	
-   /**
-    * Main.
-    * @param args - args.
-    */
-	
-	public static void main(String[] args) {
-		System.out.println("Hello World");
-	}
-	
+    /**
+     * Main.
+     * @param args - args.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+
+    /**
+     * Method echo.
+     * @param Dmitry Golyshkin
+     * @return Echo plus your name.
+     */
+    public String echo(String name) {
+        return "Echo, echo, echo : " + name;
+    }
 }
---
+++ chapter_001/src/main/java/ru/job4j/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for calculate task.
+*
+* @author Dmitry Golyshkin (mailerema@gmail.com)
+* @version $Id$
+* @since 10.05.2018
+*/
+package ru.job4j;
