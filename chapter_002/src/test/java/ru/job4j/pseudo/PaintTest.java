575f2cc9d4cc5fd281b11d64589d43ec169abc18
Dmitry Golyshkin <dmgoly@gmail.com>
2019-06-23 01:53:07 +0300

    5. Рефакторинг теста @Before @After

--- chapter_002/src/test/java/ru/job4j/pseudo/PaintTest.java
+++ chapter_002/src/test/java/ru/job4j/pseudo/PaintTest.java
@@ -1,5 +1,7 @@
 package ru.job4j.pseudo;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
@@ -12,14 +14,22 @@ import static org.junit.Assert.assertThat;
  */
 
 public class PaintTest {
+    // поле содержит дефолтный вывод в консоль.
+    private final PrintStream stdout = System.out;
+    // буфер для результата.
+    private final ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+    @Before
+    public void loadOutput() {
+        System.setOut(new PrintStream(this.out));
+    }
+    @After
+    public void backOutput() {
+        System.setOut(this.stdout);
+    }
+
     @Test
     public void whenDrawSquare() {
-        // получаем ссылку на стандартный вывод в консоль.
-        PrintStream stdout = System.out;
-        // Создаем буфур для хранения вывода.
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        //Заменяем стандартный вывод на вывод в пямять для тестирования.
-        System.setOut(new PrintStream(out));
         // выполняем действия пишушиее в консоль.
         new Paint().draw(new Square());
         // проверяем результат вычисления
@@ -35,5 +45,22 @@ public class PaintTest {
                                 toString()));
         // возвращаем обратно стандартный вывод в консоль.
         System.setOut(stdout);
+    }
+    @Test
+    public void whenDrawTriangle(){
+        // выполняем действия пишушиее в консоль.
+        new Paint().draw(new Triangle());
+        // проверяем результат вычисления
+        assertThat(
+                new String(out.toByteArray()),
+                is(
+                        new StringBuilder().
+                                append("   +   ").
+                                append("  + +  ").
+                                append(" +   + ").
+                                append("+++++++").
+                                append(System.lineSeparator()).
+                                toString()));
+
     }
 }
