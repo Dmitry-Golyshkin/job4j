9e6fc12510ecb589d5ccb9a358c3090ef8c8c886
Dmitry Golyshkin <dmgoly@gmail.com>
2019-06-24 00:13:53 +0300

    6. Доработать тесты в StartUITest проверяющие вывод данных в консоль.

--- chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
+++ chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
@@ -1,12 +1,65 @@
 package ru.job4j.tracker;
 
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
-import static org.hamcrest.core.Is.is;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
+
+/**
+ * @author Dmitry Golyshkin (dmgoly@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
 
 public class StartUITest {
     Tracker tracker = new Tracker();     // создаём Tracker
+    // поле содержит дефолтный вывод в консоль.
+    private final PrintStream stdout = System.out;
+    // буфер для результата.
+    private final ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+    @Before
+    public void loadOutput() {
+        System.setOut(new PrintStream(this.out));
+    }
+    @After
+    public void backOutput() {
+        System.setOut(this.stdout);
+    }
+
+    private String menu = "Меню.\n" +
+            "0.ADD new Item.\n" +
+            "1.Show all Item.\n" +
+            "2.Edit Item.\n" +
+            "3.Delete item.\n" +
+            "4.Find Item by Id.\n" +
+            "5.Find Item by Name.\n" +
+            "6.Exit.";
+
+    @Test
+    public void whenFindById(){
+        Item item = tracker.add(new Item("test","desc"));
+        Input input = new StubInput(new String[]{"4", item.getId(),"6"});
+        new StartUI(input,tracker).init();
+        assertThat(
+                this.out.toString(),
+                is(new StringBuilder().
+                        append(menu + "\n\n" +
+                                "Item ID " + item.getId() + " Found.\n" +
+                                "Name Item: " + item.getName() +"\n" +
+                                "Description : " + item.getDescription() + "\n" +
+                                menu + "\n"
+                        ).
+                        append(System.lineSeparator()).
+                        toString()));
+
+    }
 
 
     @Test
